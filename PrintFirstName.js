ec59ff0483e3eff6e7f052c5568cd62846f2f6bb
Manish <mkk725050@gmail.com>
2026-02-14 13:47:12 +0530

    Signal Code

---
+++ PrintFirstName.js
@@ -0,0 +1,3 @@
+let name="Manish";
+ name[0];
+
---
+++ TraficLight.js
@@ -0,0 +1,14 @@
+let color="green";
+
+if(color==="red")
+{
+    console.log("Stop Here Because Signal is Red ");
+}
+if(color==="yello")
+{
+    console.log("Go Slow Because Signal is Yellow");
+}
+if(color==="green")
+{
+    console.log("Go Because Signal is Green Now");
+}
