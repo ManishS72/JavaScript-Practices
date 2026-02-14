0139c04283b57664b89f91ec520ceaf0ec73c176
Manish <mkk725050@gmail.com>
2026-02-14 14:52:14 +0530

    Simple Switch Case Program

---
+++ PrintMonths.js
@@ -0,0 +1,20 @@
+
+let quarter=1;
+
+switch(quarter)
+{
+    case 1 :
+        console.log("January,February,March");
+        break;
+        case 2:
+            console.log("April,May,June");
+            break;
+            case 3:
+                console.log("July,August,September");
+                break;
+                case 4:
+                    console.log("October,Noveber,December");
+                    break;
+                    default:
+                        console.log("Invalid Input");
+}
