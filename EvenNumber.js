33b62105fce1eb480aa0fc2a9ef09cdfd4752eb6
Manish <mkk725050@gmail.com>
2026-02-18 01:26:04 +0530

    Using for loop only here

---
+++ EvenNumber.js
@@ -0,0 +1,15 @@
+
+let num=20;
+
+console.log("Forward Form");
+
+for(let i=2; i<=20; i=i+2)
+{
+    console.log(i);
+}
+console.log("Backward Form");
+
+for(let i=20; i>=2; i=i-2)
+{
+    console.log(i);
+}
