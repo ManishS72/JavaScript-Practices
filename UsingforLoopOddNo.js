733b8c6bfa7cc9ed2454d1ad127d6d3b6ed637ef
Manish <mkk725050@gmail.com>
2026-02-18 01:28:50 +0530

    forward and backeward form

--- UsingforLoopOddNo.js
+++ UsingforLoopOddNo.js
@@ -1,7 +1,16 @@
 
 let num=10;
 
+console.log("Forward form");
+
 for(let i=1; i<=10; i=i+2)
+{
+    console.log(i);
+}
+
+console.log("Backward form");
+
+for(let i=10; i>=1; i=i-2)
 {
     console.log(i);
 }
