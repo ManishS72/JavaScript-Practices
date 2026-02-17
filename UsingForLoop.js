cc1c892318a268122a21f2db28ea41ed505f229f
Manish <mkk725050@gmail.com>
2026-02-18 01:16:57 +0530

    simple loop

---
+++ UsingForLoop.js
@@ -0,0 +1,6 @@
+
+let name="Manish Sharma";
+for(let i =1; i<=5; i++)
+{
+    console.log(name);
+}
---
+++ Vowels.js
@@ -0,0 +1,11 @@
+let ch='a';
+ch=ch.toLowerCase();
+
+if( ch === 'a' || ch === 'e' || ch === 'i' || ch === 'o' || ch === 'u')
+{
+    console.log("Print Vowels")
+}
+else
+{
+    console.log("Consonant");
+}
