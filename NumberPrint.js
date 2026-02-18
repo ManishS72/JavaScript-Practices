2efcd1acc67024a6880a7cbd1b14fc0c59848a33
Manish <mkk725050@gmail.com>
2026-02-18 15:38:04 +0530

    small change

--- NumberPrint.js
+++ NumberPrint.js
@@ -14,3 +14,33 @@ do{
     {
         console.log(i);
     }
+
+console.log("Even Number print");
+
+for(let i=1; i<=20; i++)
+{
+    if(i % 2 === 0)
+    {
+        console.log(i);
+    }
+}
+
+console.log("odd Number");
+
+let num1=20;
+
+for(let i=1; i<=20; i++)
+{
+    if(i % 2 !== 0)
+    {
+        console.log(i);
+    }
+}
+
+console.log("Multiplicationn from here");
+
+let num2 = 5;
+for(let i=1; i<=10; i++)
+{
+    console.log(i*num2);
+}
