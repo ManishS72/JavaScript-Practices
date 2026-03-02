c0713a92770810636598a004883d72c45b6ed3a9
Manish <mkk725050@gmail.com>
2026-03-03 01:07:33 +0530

    String Practices

---
+++ TrimUsing.js
@@ -0,0 +1,23 @@
+
+let name ="Manish";
+
+let password = prompt("Create a password");
+console.log(name.trim());
+
+console.log(name.toUpperCase());
+
+console.log(name.toLowerCase());
+
+let msg = "Manish Sharma ";
+
+console.log(msg.trim().toUpperCase());
+
+let str="Hello";
+
+console.log(str.repeat(4));
+
+
+let str1="ilovecoding";
+
+console.log(str1.slice(5));
+console.log(str1.replace("love","do"));
